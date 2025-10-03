e1bc491ed86094e305eb5c5132b1f5b8df255f62
douniacodes <dounia.codes@gmail.com>
2025-10-03 13:51:08 +0200

    indexation fixes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          { type: 'host', value: 'douniacodes.com' },
+        ],
+        destination: 'https://www.douniacodes.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
